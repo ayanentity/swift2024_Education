87f3f8c1b8f1b00d6c124936d956189241d0bfdd
ayanentity <chikinnanman14@gmail.com>
2024-07-24 18:04:08 +0900

    a

---
+++ hellogit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int a,b;
+    int sum;
+    printf("足し算するよ\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    sum = a + b;
+    printf("ふたつの合計は%dです。\n",sum);
+
+    return 0;
+}
